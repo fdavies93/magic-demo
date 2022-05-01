e77fb3bbe5de5ff9f007cb3e45d6f7616c6208f0
frank <fdavies93@gmail.com>
2022-05-01 12:45:59 +0800

    Added README and license; changed file names to be a bit more logical; cleaned up setup code in game.py.

---
+++ README.md
@@ -0,0 +1,36 @@
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/fd93) [![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-orange.svg)](https://creativecommons.org/licenses/by-sa/4.0/)
+
+# MAGIC Text Adventure Framework
+
+MAGIC, as the name suggests, is a framework for writing console-based text adventures. It leans towards the style of traditional text adventures or pen-and-paper RPGs and was originally conceived as a way for me to quickly test different logics for a pen-and-paper system I thought about many years ago -- without using dice, game pieces, etc.
+
+If you want to understand the way the framework works, the best way is to dive into game.py, the basic demo, and take a look around.
+
+If you want to contribute to the framework code, you can file a pull request or chat with me directly. I'm a regular on the ["Programmer's Hangout"](https://discord.com/invite/programming) Discord.
+
+## Design Goals
+
+The framework aims to be unopinionated, minimal and very extensible. RPG stats are not included in the framework, nor are basic verbs like "look" or "move" (although they will be in game.py). Even the basic concept of "a room" is actually just a state in an object's state dictionary; NPCs will merely be GameObjects with minds attached to them. This is a deliberate choice to solve several architecture issues with old-school MUD systems written in C.
+
+Hard-programming features like "look" into a framework requires objects to have an ever-increasing number of flags such as "visible?" and callbacks like "reaction_to_counter_attack", ultimately leading to spaghetti code. On the other hand, while not implementing verbs into a framework leads to more verbose code, end-user developers can abstract away the complexity themselves while keeping the basic framework pristine.
+
+**Because the framework is relatively minimal, it's recommended you implement your own methods to create basic objects and do other basic admin. The reference (game.py) will contain these.**
+
+## Current Project Layout
+* magic-battle-sim is a naive implementation of the basic concepts
+* magic-rpg is a more sophisticated implementation 
+* game.py is a basic demo designed to show how to implement game concepts using magic-rpg.py. It also serves as the main way of testing new features.
+
+## Current Priorities - Framework
+* Refactoring the I/O system from basic Python with Rich to use ncurses. This will allow non-blocking input and lay the groundwork for more involved emergent gameplay and smart NPCs.
+    * Ideally fully decoupling I/O from the specific technology used for I/O. This will allow the framework to host MUDs with proper work put into integrating it with web technology.
+* Implementing a tick system to allow NPCs to do stuff by themselves, rather than just reacting to player input.
+* Refactoring skills to allow skills to be used by NPCs.
+* Refactoring skill help system to allow nesting of skill types (probably instantaneous vs. time-consuming in the reference implementation)
+* Adding a save / load system for current game state (but not skills, reactions, or minds).
+* Writing I/O for web hosted version.
+
+## Current Priorities - Game.py
+* Implementing exit-type objects which other objects can "go" to in order to change their current location.
+* Implementing basic combat between actors using a stat system.
+* Implementing a basic inventory and item system.
--- game.py
+++ game.py
@@ -2,168 +2,75 @@ from rich import print
 from rich.table import Table
 import uuid
 from typing import Callable, Union, Any
+from magic_rpg import *
 
-class GameObject:
-    def __init__(self):
-        self.id : str = uuid.uuid4()
-        self.states : dict = dict() # keys strings to some other data, usually attached / managed by skills
-        self.skills : set = set() # set of *ids* of available skills
-        self.reactions : set = set() # set of *ids* of reactions
-
-class Skill:
-    def __init__(self, name : str, description : str = "Some skill.", synonyms : list[str] = [], on_parsed : Callable[["Game", list[str], str], Any] = None):
-        self.id : str = uuid.uuid4()
-        self.name : str = name
-        self.description : str = description
-        self.synonyms : str = synonyms
-        self.on_parsed = on_parsed
-
-class Reaction:
-    def __init__(self, reaction_to : str, handle):
-        self.id : str = uuid.uuid4()
-        self.reacting_to : str = reaction_to # id, not name
-        self.callback = handle
+def create_object(game : Game, name: str, description: str, location: str = None):
+    new_obj = GameObject()
+    new_obj.states["name"] = name
+    new_obj.states["description"] = description
+    if location != None:
+        new_obj.states["location"] = location
+    game.add_object(new_obj)
+    return new_obj
 
 
-def help(game : "Game"):
-    tbl = Table(title="Commands")
-    tbl.add_column("Command", justify="center", style="bright_cyan")
-    tbl.add_column("Description", justify="center")
-    for cmd in Game._default_commands:
-        tbl.add_row(cmd, Game._default_commands[cmd][0])
-    print(tbl)
-
-def exit_game(game : "Game"):
-    game.exit = True
-
-def show_skills(game : "Game"):
-    player : GameObject = game.game_objects[game.player_id]
-    tbl = Table(title="Skills")
-    tbl.add_column("Skill", justify="center", style="bright_cyan")
-    tbl.add_column("Description", justify="center")
-    for skill_id in player.skills:
-        skill : Skill = game.skills.get(skill_id)
-        tbl.add_row(skill.name, skill.description)
-    print(tbl)
+def skill_look(game : "Game", args, skill_id, caller_id): # that info can kind of be bundled to an InvocationContext or similar
+    caller : GameObject = game.get_by_id(caller_id)
+    location = game.get_by_id(caller.states["location"])
+    same_location = game.get_by_state("location",lambda loc : loc == caller.states["location"])
+    location_data = game.react_to(caller.id, skill_id, location.id) # caller optional
+    object_datas = [ game.react_to(caller.id, skill_id, y.id) for y in same_location ]
     
-def do_nothing(game: "Game"):
-    pass
+    if len(args) > 1:
+        for obj in object_datas:
+            if obj["name"].lower() == args[1].lower() or (obj["synonyms"] != None and args[1].lower() in obj["synonyms"]):
+                print(f"[yellow]{obj['name']}[/]")
+                print(f"{obj['description']}")
+                return
+    object_string = ', '.join([ obj["name"] for obj in object_datas ])
+    print( f"[yellow]{location_data['name']}[/]")
+    print( f"{location_data['description']} You can see {object_string} here.")
 
+def skill_create(game : "Game", args, skill_id, caller_id):
+    if len(args) < 3:
+        print("[bright_cyan]create[/] NAME DESCRIPTION")
+        return
+    caller : GameObject() = game.get_by_id(caller_id)
+    location = game.get_by_id(caller.states["location"])
 
-class Game:
-
-    _default_commands = {"help": ("See this help message", help), "quit": ("Exit the game.", exit_game), "skills": ("Show your available skills (things you can do).", show_skills)}
-
-    def __init__(self):
-        self.game_objects : dict[GameObject] = dict()
-        self.skills : dict[str, Skill] = dict()
-        self.reactions : dict[str, Reaction] = dict()
-        self.before_start : Callable[["Game"], None] = do_nothing
-        self._skill_parse_dict : dict[str, str] = dict()
-        self._reaction_parse_dict : dict[str, list[str]] = dict()
-        self.exit : bool = False
-
-    def split_args(raw: str) -> list[str]:
-        i = 0
-        start = 0
-        buffer = ""
-        quoting = False
-        arg_list = []
-        while i < len(raw):
-            buffer += raw[i]
-            if raw[i] == " " and not quoting:
-                arg_list.append(buffer[:-1])
-                start = i+1
-                buffer = ""
-            if raw[i] in "\"\'" and not quoting:
-                quoting = True
-                start = i
-                buffer = ""
-            elif raw[i] in "\"\'" and quoting:
-                quoting = False
-                buffer = buffer[:-1]
-            i += 1
-        if len(buffer) > 0:
-            arg_list.append(buffer)
-        return arg_list
-
-
-    def parse(self, raw : str):
-        split = Game.split_args(raw)
-        if len(split) > 0 and split[0] in Game._default_commands:
-            Game._default_commands[split[0]][1](self)
-        elif len(split) > 0 and split[0] in self._skill_parse_dict:
-            # this is STILL wrong because we need the player object to be set up as a receiver for inputs
-            skill_id = self._skill_parse_dict.get(split[0])
-            self.skills.get(skill_id).on_parsed(self, split, skill_id, self.player_id)
-
-    def get_by_state(self, state_id, eval_fn):
-        # this builds a pretty strong case for having a state manager rather than having GameObjects handle their own state
-        # because we want to be able to index, update, and search for state (location is a state) independently of whatever
-        # a given GameObject thinks it ought to be doing
-        return [
-            y
-            for y in self.game_objects.values()
-            if state_id in y.states
-            if eval_fn(y.states[state_id])
-        ]
-
-    def get_by_id(self, id_ : str):
-        return self.game_objects.get(id_)
-
-    def add_skill(self, skill : Skill):
-        self.skills[skill.id] = skill
-        self._skill_parse_dict[skill.name.lower()] = skill.id
-        for synonym in skill.synonyms:
-            self._skill_parse_dict[synonym.lower()] = skill.id
-
-    def add_object(self, obj : GameObject):
-        self.game_objects[obj.id] = obj
+    new_obj = create_object(game, args[1], args[2], location.id)
     
-    def add_reaction(self, reaction : Reaction):
-        self.reactions[reaction.id] = reaction
-        reaction_list = self._reaction_parse_dict.get(reaction.reacting_to)
-        if reaction_list != None:
-            reaction_list.append(reaction.id)
-        else:
-            reaction_list = [reaction.id]
-        self._reaction_parse_dict[reaction.reacting_to] = reaction_list
+    generic_look : str = game.get_reactions("look")[0] # need a better unique identifier for things like this
 
-    def get_reactions(self, reacts_to : str):
-        skill_id = self._skill_parse_dict.get(reacts_to)
-        if skill_id == None:
-            return []
-        reaction_ids = self._reaction_parse_dict.get(skill_id)
-        if reaction_ids == None:
-            return []
-        return reaction_ids
+    new_obj.reactions.add(generic_look)
 
-    def react_to(self, actor_id, skill_id, target_id):
-        target : GameObject = self.get_by_id(target_id)
+def reaction_look_visible(game : "Game", looker_id, self_id) -> dict:
+    # print ( "Calling reaction_look_visible" )
+    myself : GameObject = game.get_by_id(self_id)
+    return { "name": myself.states.get("name"), "description": myself.states.get("description"), "synonyms": myself.states.get("synonyms") }
 
-        target_reactions = [ 
-            self.reactions.get(reaction_id)
-            for reaction_id in target.reactions
-            if self._reaction_parse_dict.get(skill_id) != None
-        ]
 
-        final_targets = [
-            reaction
-            for reaction in target_reactions
-            if reaction.id in self._reaction_parse_dict.get(skill_id)
-        ]
+def game_setup(game : Game):
 
-        if len(final_targets) > 1:
-            print(f"[red]Warning: more than one reaction to {skill_id} in {actor_id}. Only executing the first one.[/red]")
-        if len(final_targets) > 0:
-            return final_targets[0].callback(self, actor_id, target_id)
-        return None
+    look_skill = Skill("look", on_parsed=skill_look)
+    create_skill = Skill("create", on_parsed=skill_create)
+    look_reaction_visible = Reaction(look_skill.id, reaction_look_visible)
 
-    def start(self):
-        # setup code
-        self.before_start(self)
-        print("[bright_cyan]Try help if you need help.[/bright_cyan]")
-        while not self.exit:
-            raw = input("> ")
-            # print(Game.split_args(raw))
-            self.parse(raw)
+    room = create_object(game, "A room", "A small room")
+    player = create_object(game, "Hero", "You look very handsome.", room.id)
+    box = create_object(game, "Box", "It's a box", room.id)
+    
+    game.add_skill(look_skill)
+    game.add_skill(create_skill)
+    game.add_reaction(look_reaction_visible)
+    player.skills.add(look_skill.id)
+    player.skills.add(create_skill.id)
+    player.reactions.add(look_reaction_visible.id)
+    room.reactions.add(look_reaction_visible.id)
+    box.reactions.add(look_reaction_visible.id)
+    game.player_id = player.id
+
+if __name__ == "__main__":
+    gm = Game()
+    gm.before_start = game_setup
+    gm.start()
