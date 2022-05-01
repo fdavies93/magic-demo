7150b3ae28ab9e9c54f930e28dc54bd3ba7e3640
frank <fdavies93@gmail.com>
2022-05-02 01:21:55 +0800

    Wrote loopback method for CursesIO

--- io_test.py
+++ io_test.py
@@ -1,5 +1,8 @@
 from magic_io import CursesIO
 
+def echo(interface : "CursesIO", input_ : str):
+    interface.add_output(input_)
+
 if __name__ == "__main__":
-    io = CursesIO()
+    io = CursesIO(on_input=echo)
     io.start()
--- magic_io.py
+++ magic_io.py
@@ -2,10 +2,12 @@ import curses
 from curses import ascii
 import time
 
+
 class CursesIO():
-    def __init__(self, tick_time : float = 0.0625, input_prefix = "> "): # i.e. 1/32 of a second
+    def __init__(self, tick_time : float = 0.0625, input_prefix = "> ", on_input = None): # i.e. 1/32 of a second
         self.stdscr = None
         self.msg_buffer = []
+        self.quit = False
         self.output_scr = None
         self.input_scr = None
         self.tick_time = tick_time
@@ -14,6 +16,8 @@ class CursesIO():
         self.output_buffer = []
         self.input_history = []
         self.history_pos = -1
+        self.refresh_output = False
+        self.on_input = on_input
 
     def init_windows(self, stdscr):
         if stdscr != None:
@@ -25,28 +29,32 @@ class CursesIO():
         self.stdscr.refresh()
         self.output_scr.refresh()
         self.input_scr.refresh()
-        time.sleep(self.tick_time)
         
+
+    def add_output(self, output):
+        self.output_buffer.append(output)
+        self.refresh_output = True
 
     def main_loop(self):
         cur_key = None
-        while True:
+        while not self.quit:
+
+            if self.refresh_output:
+                self.output_scr.clear()
+                to_print = self.output_buffer[1-curses.LINES:]
+                self.output_scr.addstr("\n".join(to_print))
+                self.output_scr.refresh()
+                self.refresh_output = False
+                self.input_scr.refresh()
+
             key_ascii = self.stdscr.getch()
             key_str = ascii.unctrl(key_ascii)
             if (key_ascii != -1):
                 if key_ascii == curses.KEY_ENTER or key_ascii == 10 or key_ascii == 13:
-
                     # if it will move cursor outside the edge of the window
-
-                    self.output_buffer.append(self.input_buffer)
-
-                    self.output_scr.clear()
-                    to_print = self.output_buffer[1-curses.LINES:]
-                    self.output_scr.addstr("\n".join(to_print))
-
                     self.input_history.append(self.input_buffer)
-
-                    self.output_scr.refresh()
+                    if self.on_input != None:
+                        self.on_input(self, self.input_buffer)
                     self.input_buffer = ""
                 elif key_ascii == curses.KEY_BACKSPACE:
                     self.input_buffer = self.input_buffer[:-1]
@@ -55,6 +63,7 @@ class CursesIO():
                 self.input_scr.clear()
                 self.input_scr.addstr(self.input_prefix + self.input_buffer)
                 self.input_scr.refresh()
+            time.sleep(self.tick_time)
 
     @staticmethod
     def _start(stdscr, io : "CursesIO"):
