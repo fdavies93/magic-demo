e0757936d3016dc0c627ab37a60ccbaa698eccf5
frank <fdavies93@gmail.com>
2022-05-02 14:48:19 +0800

    Added scrolling by using the tab key

--- magic_io.py
+++ magic_io.py
@@ -4,6 +4,39 @@ import time
 
 
 class CursesIO():
+
+    @staticmethod
+    def on_enter(io_):
+        io_.input_history.append(io_.input_buffer)
+        if io_.on_input != None:
+            io_.on_input(io_, io_.input_buffer)
+        io_.input_buffer = ""
+    
+    @staticmethod
+    def on_backspace(io_):
+        io_.input_buffer = io_.input_buffer[:-1]
+
+    @staticmethod
+    def on_tab(io_):
+        if not io_.scrolling:
+            io_.scrolling = True
+        else:
+            io_.can_refresh_output = True
+            io_.scrolling = False
+            io_.cursor_location = 0
+
+    @staticmethod
+    def on_up(io_):
+        if io_.scrolling and io_.cursor_location < (len(io_.output_buffer) - (curses.LINES - 2)):
+            io_.cursor_location += 1
+            io_.can_refresh_output = True
+    
+    @staticmethod
+    def on_down(io_):
+        if io_.scrolling and io_.cursor_location > 0:
+            io_.cursor_location -= 1
+            io_.can_refresh_output = True
+
     def __init__(self, input_prefix = "> ", on_input = None, logfile = None): # i.e. 1/32 of a second
         self.stdscr = None
         self.msg_buffer = []
@@ -15,9 +48,20 @@ class CursesIO():
         self.output_buffer = []
         self.input_history = []
         self.history_pos = -1
-        self.refresh_output = False
+        self.can_refresh_output = False
         self.on_input = on_input
         self.logfile = logfile
+        self.cursor_location = 0
+        self.scrolling = False
+        self.key_presses = {
+            curses.KEY_ENTER : CursesIO.on_enter,
+            ord('\n') : CursesIO.on_enter,
+            ord('\r') : CursesIO.on_enter,
+            curses.KEY_BACKSPACE : CursesIO.on_backspace,
+            ord('\t') : CursesIO.on_tab,
+            curses.KEY_UP : CursesIO.on_up,
+            curses.KEY_DOWN : CursesIO.on_down
+        }
 
     def __enter__(self):
         self.init_windows()
@@ -53,68 +97,33 @@ class CursesIO():
 
     def add_output(self, output):
         self.output_buffer.append(output)
-        self.refresh_output = True
+        self.can_refresh_output = True
+
+    def refresh_output(self):
+        self.output_scr.clear()
+        buf_len = len(self.output_buffer)
+        start = (buf_len + 1 - self.cursor_location) - curses.LINES
+        if start < 0: start = 0
+        to_print = self.output_buffer[ start : buf_len - self.cursor_location ]
+        self.output_scr.addstr("\n".join(to_print))
+        self.output_scr.refresh()
+        self.can_refresh_output = False
+        self.input_scr.refresh()
 
     def poll(self):
-        self.log("polling")
-
-        if self.refresh_output:
-            self.output_scr.clear()
-            to_print = self.output_buffer[1-curses.LINES:]
-            self.output_scr.addstr("\n".join(to_print))
-            self.output_scr.refresh()
-            self.refresh_output = False
-            self.input_scr.refresh()
-            self.log("Refreshed output")
-
+        if self.can_refresh_output:
+            self.refresh_output()
         key_ascii = self.stdscr.getch()
         key_str = ascii.unctrl(key_ascii)
         
         if (key_ascii != -1):
-            if key_ascii == curses.KEY_ENTER or key_ascii == 10 or key_ascii == 13:
-                # if it will move cursor outside the edge of the window
-                self.input_history.append(self.input_buffer)
-                if self.on_input != None:
-                    self.on_input(self, self.input_buffer)
-                self.input_buffer = ""
-            elif key_ascii == curses.KEY_BACKSPACE:
-                self.input_buffer = self.input_buffer[:-1]
-            else:
+            if key_ascii in self.key_presses:
+                self.key_presses.get(key_ascii)(self)
+            elif not self.scrolling:
                 self.input_buffer += key_str
             self.input_scr.clear()
-            self.input_scr.addstr(self.input_prefix + self.input_buffer)
+            input_str = self.input_prefix
+            if (self.scrolling):
+                input_str = "[SCROLL]"
+            self.input_scr.addstr(input_str + self.input_buffer)
             self.input_scr.refresh()
-            self.log("keypress detected")
-
-    # def main_loop(self):
-    #     cur_key = None
-    #     while not self.quit:
-
-    #         if self.refresh_output:
-    #             self.output_scr.clear()
-    #             to_print = self.output_buffer[1-curses.LINES:]
-    #             self.output_scr.addstr("\n".join(to_print))
-    #             self.output_scr.refresh()
-    #             self.refresh_output = False
-    #             self.input_scr.refresh()
-
-    #         key_ascii = self.stdscr.getch()
-    #         key_str = ascii.unctrl(key_ascii)
-    #         if (key_ascii != -1):
-    #             if key_ascii == curses.KEY_ENTER or key_ascii == 10 or key_ascii == 13:
-    #                 # if it will move cursor outside the edge of the window
-    #                 self.input_history.append(self.input_buffer)
-    #                 if self.on_input != None:
-    #                     self.on_input(self, self.input_buffer)
-    #                 self.input_buffer = ""
-    #             elif key_ascii == curses.KEY_BACKSPACE:
-    #                 self.input_buffer = self.input_buffer[:-1]
-    #             else:
-    #                 self.input_buffer += key_str
-    #             self.input_scr.clear()
-    #             self.input_scr.addstr(self.input_prefix + self.input_buffer)
-    #             self.input_scr.refresh()
-    #         time.sleep(self.tick_time)
-
-    # def start(self):
-    #     self.init_windows(stdscr)
