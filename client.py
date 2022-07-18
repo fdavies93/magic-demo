383693aa653625c8b928c60a57a8441b5c080121
Frank Davies <fdavies93@gmail.com>
2022-07-18 23:28:43 +0800

    Built client and basic server with websockets, including connect, disconnect, and message parameters.

--- client.py
+++ client.py
@@ -1,23 +1,67 @@
-import websockets
+import asyncio
+from websockets import client as ws
 import time
+import json
+
+import websockets
 from magic_io import *
 
 
-def help(client : "Client"):
+async def help(client : "Client", args):
     client.io.add_output("HELP")
     for cmd in Client._default_commands:
         client.io.add_output(f"{cmd} | {Client._default_commands[cmd][0]}")
 
-def exit_game(client : "Client"):
+async def exit_game(client : "Client", args):
     client.exit = True
+
+async def connect(client : "Client", args):
+    if len(args) < 3:
+        client.io.add_output("Usage: connect URI USERNAME")
+        return
+
+    if client.connection != None:
+        client.io.add_output("You're already connected to a server. Disconnect before doing anything else.")
+        return
+
+    client.io.add_output(f"Attempting connection to {args[1]}")
+    await asyncio.sleep(0.5)
+    try:
+        connection = await ws.connect(args[1])
+        client.connection = connection
+        client.cur_user = args[2]
+        await client.connection.send(json.dumps({ "type": "connect", "user": args[2] }))
+        client.io.add_output(f"Connected to {args[1]} successfully.")
+    except:
+        client.io.add_output("Couldn't connect to remote server.")
+        client.connection = None
+
+async def disconnect(client: "Client"):
+    if client.connection != None:
+        await client.connection.send(json.dumps({"type": "disconnect", "user": client.cur_user}))
+        await client.connection.close()
+        client.cur_user = None
+        client.connection = None
+
+async def disconnect_parse(client: "Client", args):
+    if client.connection != None:
+        await disconnect(client)
+        client.io.add_output("Disconnected from server.")
 
 class Client:
 
-    _default_commands = {"help": ("See this help message", help), "quit": ("Exit the game.", exit_game)}
+    _default_commands = {"help": ("See this help message", help), "quit": ("Exit the game.", exit_game), "connect": ("Connect to a server.", connect), "disconnect": ("Disconnect from a server.", disconnect_parse)}
 
     def __init__(self, tick_time = 0.0625):
         self.exit : bool = False
         self.tick_time = tick_time
+        self.connection = None
+        self.cur_user = None
+
+    @staticmethod
+    def decode_json(message : str) -> Union[str, RichText, list]:
+        obj = json.loads(message)
+
 
     @staticmethod
     def split_args(raw: str) -> list[str]:
@@ -45,23 +89,31 @@ class Client:
         return arg_list
 
 
-    def parse(self, raw : str):
+    async def parse(self, raw : str):
         split = Client.split_args(raw)
         if len(split) > 0 and split[0] in Client._default_commands:
-            Client._default_commands[split[0]][1](self)
+            await Client._default_commands[split[0]][1](self, split)
+        elif self.connection != None:
+            await self.connection.send(json.dumps({"type":"message", "data": split}))
+        
 
-    def start(self):
+    async def start(self):
         # setup code
+        parsing = set()
         with CursesIO() as self.io:
             while not self.exit:
-                # raw = input("> ")
-                self.io.poll()
+                self.io.poll() # probably need an async option for sending / retrieving data from cursesIO
                 next_input = self.io.pop_input()
                 if next_input != None:
-                # print(Game.split_args(raw))
-                    self.parse(next_input)
-                time.sleep(self.tick_time)
+                    cur_parse = asyncio.create_task(self.parse(next_input))
+                    parsing.add(cur_parse)
+                    cur_parse.add_done_callback(parsing.discard)
+
+                await asyncio.sleep(self.tick_time)
+            
+            await disconnect(self)
+        return
 
 if __name__ == "__main__":
     client = Client()
-    client.start()
+    asyncio.run(client.start())
--- server.py
+++ server.py
@@ -1,13 +1,49 @@
 import asyncio
 import websockets
+import json
+from magic_io import RichText
+
+JOIN = dict()
+disconnecting = set()
+connected = set()
+
+async def parse(event, user):
+    if event.get("type") == None:
+        return
+    if event["type"] == "disconnect":
+        disconnecting.add(user)
+    elif event["type"] == "message" and event.get("data") != None:
+        print(str(event["data"]))
 
 async def handler(websocket):
-    while True:
+    connected.add(websocket)
+    try:
         message = await websocket.recv()
-        print (message)
+        event = json.loads(message)
+
+        assert event["type"] == "connect"
+
+        user = event["user"]
+        JOIN[event["user"]] = websocket
+
+        print ( f"User {event.get('user')} connected to server." )
+
+        while not (user in disconnecting):
+            message = await websocket.recv()
+            event = json.loads(message)
+            await parse(event, user)
+
+    finally:
+        print (f"User {user} disconnecting from server.")
+        await websocket.close()
+        disconnecting.remove(user)
+        connected.remove(websocket)
+        del JOIN[event["user"]]
     
 async def main():
-    async with websockets.serve(handler, "", 8001):
+    port = 8001
+    async with websockets.serve(handler, "", port):
+        print(f"Opening server on port {port}.")
         await asyncio.Future()
 
 if __name__ == "__main__":
